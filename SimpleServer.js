54da8782020109d0fa32ece99643d17e83ce928c
Tomas <budzikt@gmail.com>
2017-06-11 23:25:28 +0200

    express added

---
+++ SimpleServer.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var app = require('express');
+var portNum = 8080;
+http.createServer(function(request, response) {
+response.writeHead(200);
+request.pipe(response);
+}).listen(portNum)
+console.log('Server listening on port ' + portNum);
