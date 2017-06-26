4b063ab4118117e74264b654761db016260d7d3d
Tomas <budzikt@gmail.com>
2017-06-26 22:29:05 +0200

    url body parser fails

--- SimpleServer.js
+++ SimpleServer.js
@@ -8,31 +8,47 @@ var bodyparser = require('body-parser');
 var morgan = require('morgan');
 var vhost = require('vhost');
 var compression = require('compression');
+var methodoverride = require('method-override');
 //Importy wlasne
 var b_util = require('./utils');
 
-//Konstruktor express
-var app = express();
+app = express();
 
 //Konfiguracja express
 var portNum = 8010;
+app.set('port', 8010);
 app.set('myDebug', true);
+app.set('vhosting', false);
 
 //Oprogramowanie pośredniczące
 app.use(compression()); // Kompresja odpowiedzi dla klienta
+//app.use(bodyparser.urlencoded({ extended: true })); // Analiza zapytania URL
+//app.use(bodyparser.json());  // Analiza JSON zapytania
 app.use(cookieParser());  // analiza ciasteczek zapytania
-app.use(bodyparser.urlencoded({ extended: true })); // Analiza zapytania URL
-app.use(bodyparser.json());  // Analiza JSON zapytania
+app.use(methodoverride('X-HTTP-Method-Override'));
 app.use(morgan('tiny')) // Logger zdarzen
-
 app.use(b_util.reqUserParse); //biblioteki wlasne
 
+//Virtual hosts
+// var apiApp = express(); // Osobna aplikacja do obslugi REST api
+// var vhApi = vhost('api.budzikt.pl', apiApp);
+// app.use(vhApi);
+
 //Rooting
+//Main page 
 app.get('/', function(req,res){ 
-    var query = req.query; 
-    console.log(b_util.isJsonEmpty(query));
     res.end("Main");
 })
+
+app.post('/', function(req,res){ 
+    var query = req.query
+    res.send(JSON.stringify(query));
+})
+
+app.delete('/', function(req,res){
+    res.end("Delete")
+})
+
 
 app.get('/items', function(req,res){
     res.end("Przedmioty");
@@ -41,7 +57,7 @@ app.get('/items', function(req,res){
 //Plug additiona router for API requiests
 app.use('/api', require('./routes/api').apiRouter);
 
-app.listen(portNum, function(){
-    console.log("Serwer nasluchje na porcie 8010");
+app.listen(app.get('port'), function(){
+    console.log("Serwer nasluchje na porcie " + app.get('port'));
     console.log("Sciezka projektu to: " + __dirname);
 })
