718fa37434c70cdf58724b818bbe2c9f31bf0a02
Tomas <budzikt@gmail.com>
2017-07-06 23:38:21 +0200

    reordered DbReloader

--- dbHandler.js
+++ dbHandler.js
@@ -1,35 +1,30 @@
 var MongoDb = require('mongodb');
 var assert = require('assert');
+
 var jsonTestItems = require('./dbTestData.json');
 var jsonTestUsers = require('./dbTestUsers.json')
 
 var MongoClient = MongoDb.MongoClient;
 var DbUrl = 'mongodb://localhost:27017/shop';
 
-// Use connect method to connect to the server
-
-
-function RevertDbData(client, url){
+function RevertDbData(client, url, collection, dataIn={}, callback){
   client.connect(DbUrl, function(err, db) {
     assert.equal(null, err);
-    console.log("Polaczono z baza danych");
-    insertDocuments(db, 'ShopItems', jsonTestItems['items'], function(result) {
-        db.close();
+    console.log("Polaczono z baza danych " + db.databaseName);
+    insertDocuments(db, collection, dataIn, function(result) {
+        callback(db, collection, db.close);
     });
   });
 };
 
-var insertDocuments = function(db, collection='ShopItems', dataset={}, callback) {
-  
-  db.dropCollection('ShopItems', function(){
+var insertDocuments = function(db, collection, dataset={}, callback) {
+  db.dropCollection(collection, function(){
     console.log('Usunueto cala kolekcje, dodaje dane startowe');
-    var collectionItems = db.collection('ShopItems');
-    
+    var collectionItems = db.collection(collection);
     //Add date to each JSON item
     for (var i = 0; i< dataset.length; i++){
       dataset[i].dateAdded = new Date(2017,7,1+i,5);
     }
-    
     collectionItems.insertMany(dataset, function(err, result) {
       assert.equal(err, null);
       console.log("Dodano swieze dane");
@@ -38,16 +33,15 @@ var insertDocuments = function(db, collection='ShopItems', dataset={}, callback)
   });
 }
 
-var findDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('ShopItems');
-  // Find some documents
+var findDocuments = function(db, collection) {
+  var collection = db.collection(collection);   
   collection.find({}).toArray(function(err, docs) {
     assert.equal(err, null);
     console.log("Znaleziono " + docs.length + " rekordy:");
     console.log(docs)
-    callback(docs);
+    db.close();
   });
 }
 
-RevertDbData(MongoClient, DbUrl);
+RevertDbData(MongoClient, DbUrl, 'ShopItems', jsonTestItems['items'], findDocuments);
+RevertDbData(MongoClient, DbUrl, 'ShopUsers', jsonTestUsers['users'], findDocuments);
