ad5a2a6c6663a2b6cd8206e1a401d8972eeed352
Tomas <budzikt@gmail.com>
2017-07-23 01:02:05 +0200

    get/set products

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,50 @@
+var MongoDb = require('mongodb');
 var apiRouter = require('express').Router();
 
-apiRouter.get('/:id', function(req,res){
+apiRouter.get('/items/:id', function(req,res){
     var reqId = req.params.id;
-    if(req.app.get('myDebug')){
-        console.log('GET request with ID ' + reqId);
-    }
-    console.log('Non debug msg');
+    app.locals.dataBind = app.locals.dataBind || {}; 
+    var MongoClient = MongoDb.MongoClient;
+    var url = 'mongodb://localhost:27017/shop';
+    
+    MongoClient.connect(url, function(err, db){
+        var items = db.collection('ShopItems');
+        var id;
+        try{
+        id =new MongoDb.ObjectID(reqId);
+        } catch(err){
+            id = parseInt(reqId);
+        }
+        items.find({_id : id}).toArray(function(err, items){
+            if(err) {console.log("Error at shop"); res.send(err); return;}
+            res.json(items[0]);
+        });
+    });
 });
+
+apiRouter.post('/items/:id', function(req,res){
+    app.locals.dataBind = app.locals.dataBind || {}; 
+    var MongoClient = MongoDb.MongoClient;
+    var url = 'mongodb://localhost:27017/shop';
+
+    body = req.body;
+    var id = body.id;//new MongoDb.ObjectID(body.id);
+    
+    MongoClient.connect(url, function(err, db){
+        var items = db.collection('ShopItems');
+        items.find({_id : id}).toArray(function(err, docs){
+            if(err) {console.log("Error at shop"); res.send(err); return;}
+            if(docs.length != 0){
+                res.end("Ten obiekt juz istnieje");
+            }
+            body._id = body.id;
+            delete body.id;
+            items.insertOne(body, function(err, result){
+                db.close();
+
+            });
+        });
+    });
+})
 
 exports.apiRouter = apiRouter;
